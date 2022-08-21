b5b3a62b7dda78157f0b41f8fb33419a80d0c077
Yang Yuan <yangyuan@Yangs-MacBook-Pro.local>
2022-08-21 12:54:15 +0800

    upload initial project

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,20 @@
+import "./ExpenseItem.css";
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div>
+        <div className="expense-date_month">{month}</div>
+        <div className="expense-date_day">{day}</div>
+        <div calssName="expense-date_year">{year}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
